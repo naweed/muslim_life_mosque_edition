7da9de737e785631b92363b9585e295a37f97871
Naweed Akram <naweed@xgeno.com>
2025-05-09 17:58:34 +0100

    Commit

--- src/lib/Views/onboarding_page.dart
+++ src/lib/Views/onboarding_page.dart
@@ -85,26 +85,34 @@ class OnboardingPage extends StackedView<OnboardingPageViewModel> {
                     child: Focus(
                       focusNode: viewModel.nextButtonFocus,
                       canRequestFocus: true,
-                      child: CupertinoButton(
-                        padding: 0.withAllPadding(),
-                        onPressed: () async {},
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            SizedBox(
-                              height: 55,
-                              width: 55,
-                              child: CircularProgressIndicator(
-                                backgroundColor: AppColors.LightIndicatorColor,
-                                valueColor: const AlwaysStoppedAnimation<Color>(AppColors.DarkGreenColor),
-                                value: (viewModel.currentPage + 1) / viewModel.OnboardingScreens.length,
+                      child: Container(
+                        padding: 16.withAllPadding(),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: AppColors.LightIndicatorColor.withValues(alpha: 0.8), width: 1.5),
+                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        ),
+
+                        child: CupertinoButton(
+                          padding: 0.withAllPadding(),
+                          onPressed: () async {},
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              SizedBox(
+                                height: 64,
+                                width: 64,
+                                child: CircularProgressIndicator(
+                                  backgroundColor: AppColors.LightIndicatorColor.withValues(alpha: 0.2),
+                                  valueColor: const AlwaysStoppedAnimation<Color>(AppColors.DarkGreenColor),
+                                  value: (viewModel.currentPage + 1) / viewModel.OnboardingScreens.length,
+                                ),
                               ),
-                            ),
-                            const CircleAvatar(
-                              backgroundColor: AppColors.DarkGreenColor,
-                              child: Icon(Icons.arrow_forward_ios_outlined, color: AppColors.LightIndicatorColor),
-                            ),
-                          ],
+                              const CircleAvatar(
+                                backgroundColor: AppColors.DarkGreenColor,
+                                child: Icon(Icons.arrow_forward_ios_outlined, color: AppColors.LightIndicatorColor),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -121,9 +129,9 @@ class OnboardingPage extends StackedView<OnboardingPageViewModel> {
   AnimatedContainer _buildDot(int index, BuildContext context, OnboardingPageViewModel viewModel) => AnimatedContainer(
     duration: AppConstants.SmallDuration.milliseconds,
     curve: Curves.easeInOut,
-    height: 8,
-    width: viewModel.currentPage == index ? 24 : 8,
-    margin: 8.withRightPadding(),
+    height: 12,
+    width: viewModel.currentPage == index ? 32 : 12,
+    margin: 12.withRightPadding(),
     decoration: BoxDecoration(
       borderRadius: BorderRadius.circular(20),
       color: viewModel.currentPage == index ? AppColors.DarkGreenColor : AppColors.LightIndicatorColor,
