fe462fd55e9560d19c7a03a0d0af123daa37ab10
Naweed Akram <naweed@xgeno.com>
2025-05-10 08:18:06 +0100

    Commit

--- src/lib/ViewControls/country_selection_page/country_display_cell.dart
+++ src/lib/ViewControls/country_selection_page/country_display_cell.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:muslim_life_mosque_edition/Framework/Extensions/padding_extensions.dart';
 import 'package:muslim_life_mosque_edition/Framework/Extensions/sized_box_extensions.dart';
-import 'package:muslim_life_mosque_edition/Framework/Extensions/widget_extensions.dart';
 import 'package:muslim_life_mosque_edition/Models/country.dart';
 import 'package:muslim_life_mosque_edition/Shared/app_colors.dart';
 import 'package:muslim_life_mosque_edition/Shared/app_constants.dart';
@@ -11,44 +11,78 @@ import 'package:muslim_life_mosque_edition/Shared/app_styles.dart';
 class CountryDisplayCell extends StatelessWidget {
   final Country country;
   final bool isSelected;
-  final void Function() onTapped;
+  final VoidCallback onTapped;
+  final FocusNode focusNode;
 
-  const CountryDisplayCell({super.key, required this.country, required this.onTapped, required this.isSelected});
+  const CountryDisplayCell({
+    super.key,
+    required this.country,
+    required this.isSelected,
+    required this.onTapped,
+    required this.focusNode,
+  });
 
   @override
-  Widget build(BuildContext context) => GestureDetector(
-    onTap: onTapped,
-    child: Container(
-      padding: (16, 16).withSymetricPadding(),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(6),
-        border: Border.all(color: AppColors.DarkGrayColor.withValues(alpha: 0.7)),
-        color: isSelected ? AppColors.DarkGreenColor.withValues(alpha: 0.65) : AppColors.LightGreenColor,
-      ),
-      width: double.infinity,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: isSelected ? AppColors.PageBackgroundColor : AppColors.AppPrimaryColor,
-                width: 0.5,
-              ),
-            ),
-            child: SvgPicture.asset(
-              "${AppConstants.FlagsPath}/${country.id!.toLowerCase()}.svg",
-              height: 18,
-              width: 24,
-            ),
+  Widget build(BuildContext context) => Focus(
+    focusNode: focusNode,
+    onKeyEvent: (node, event) {
+      if (event is KeyDownEvent) {
+        if (event.logicalKey == LogicalKeyboardKey.select || event.logicalKey == LogicalKeyboardKey.enter) {
+          onTapped();
+          return KeyEventResult.handled;
+        }
+      }
+      return KeyEventResult.ignored;
+    },
+    child: GestureDetector(
+      onTap: onTapped,
+      child: Container(
+        padding: (focusNode.hasFocus ? 8 : 0).withAllPadding(),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: focusNode.hasFocus ? AppColors.LightIndicatorColor.withValues(alpha: 0.8) : Colors.transparent,
+            width: 1.5,
           ),
-          12.toHorizontalSpacer(),
-          Text(
-            country.name!,
-            style: isSelected ? AppStyles.RegularLight16TextStyle : AppStyles.RegularDark16TextStyle,
-            overflow: TextOverflow.ellipsis,
-          ).expandWidget(),
-        ],
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        ),
+        child: Container(
+          padding: (16, 16).withSymetricPadding(),
+          decoration: BoxDecoration(
+            color: isSelected ? AppColors.DarkGreenColor.withValues(alpha: 0.65) : AppColors.LightGreenColor,
+            border: Border.all(
+              color:
+                  focusNode.hasFocus
+                      ? AppColors.LightIndicatorColor
+                      : AppColors.LightIndicatorColor.withValues(alpha: 0.5),
+              width: focusNode.hasFocus ? 2.0 : 1.0,
+            ),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: isSelected ? AppColors.PageBackgroundColor : AppColors.AppPrimaryColor,
+                    width: 0.5,
+                  ),
+                ),
+                child: SvgPicture.asset(
+                  "${AppConstants.FlagsPath}/${country.id!.toLowerCase()}.svg",
+                  height: 18,
+                  width: 24,
+                ),
+              ),
+              12.toHorizontalSpacer(),
+              Text(
+                country.name!,
+                style: AppStyles.OnboardingSubTitleTextStyle.copyWith(
+                  color: isSelected ? AppColors.LightIndicatorColor : AppColors.DarkGreenColor,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
--- src/lib/Views/country_selection_page.dart
+++ src/lib/Views/country_selection_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:muslim_life_mosque_edition/Actions/enter_button_action.dart';
 import 'package:muslim_life_mosque_edition/Framework/Extensions/device_extensions.dart';
 import 'package:muslim_life_mosque_edition/Framework/Extensions/navigation_extentions.dart';
 import 'package:muslim_life_mosque_edition/Framework/Extensions/padding_extensions.dart';
 import 'package:muslim_life_mosque_edition/Framework/Extensions/sized_box_extensions.dart';
 import 'package:muslim_life_mosque_edition/Framework/Extensions/widget_extensions.dart';
+import 'package:muslim_life_mosque_edition/Intents/enter_button_intent.dart';
 import 'package:muslim_life_mosque_edition/Shared/app_assets.dart';
 import 'package:muslim_life_mosque_edition/Shared/app_colors.dart';
 import 'package:muslim_life_mosque_edition/Shared/app_styles.dart';
@@ -22,55 +25,113 @@ class CountrySelectionPage extends StackedView<CountrySelectionPageViewModel> {
   @override
   CountrySelectionPageViewModel viewModelBuilder(BuildContext context) {
     pageViewModel = CountrySelectionPageViewModel();
+    pageViewModel.screenContext = context;
     return pageViewModel;
   }
 
   @override
   void onViewModelReady(CountrySelectionPageViewModel viewModel) async {
     await viewModel.loadData();
+    await Future.delayed(const Duration(milliseconds: 100));
+    viewModel.requestFocus();
   }
 
   @override
   Widget builder(BuildContext context, CountrySelectionPageViewModel viewModel, Widget? child) => Scaffold(
-    body: Container(
-      padding: (48, 48, 48, 48).withLTRBPadding(),
-      width: double.infinity,
-      height: double.infinity,
-      color: AppColors.AppPrimaryColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset(AppAssets.CountryImage, width: context.width * 0.3, opacity: const AlwaysStoppedAnimation(.8)),
-          48.toHorizontalSpacer(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+    body: Focus(
+      onKeyEvent: (node, event) {
+        if (event is KeyDownEvent) {
+          if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+            viewModel.moveFocusUp();
+            return KeyEventResult.handled;
+          } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+            viewModel.moveFocusDown();
+            return KeyEventResult.handled;
+          }
+        }
+        return KeyEventResult.ignored;
+      },
+      child: Shortcuts(
+        shortcuts: <LogicalKeySet, Intent>{LogicalKeySet(LogicalKeyboardKey.select): EnterButtonIntent()},
+        child: Container(
+          padding: (48, 48, 48, 48).withLTRBPadding(),
+          width: double.infinity,
+          height: double.infinity,
+          color: AppColors.AppPrimaryColor,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text("Select your Country!", style: AppStyles.MediumLight24TextStyle),
-              12.toVerticalSpacer(),
-              SuperListView.separated(
-                itemCount: viewModel.Countries.length,
-                itemBuilder: (context, index) {
-                  final country = viewModel.Countries[index];
-
-                  return CountryDisplayCell(
-                    country: country,
-                    isSelected: country.id! == viewModel.SelectedCountry,
-                    onTapped: () => viewModel.setSelectedCountry(country.id!),
-                  );
-                },
-                separatorBuilder: (context, index) => 12.toVerticalSpacer(),
-              ).expandWidget(),
-              12.toVerticalSpacer(),
-              PageButton(
-                isEnabled: viewModel.SelectedCountry != "",
-                text: "Next",
-                onPressed: () => context.push(CitySelectionPage(selectedCountry: viewModel.SelectedCountry)),
+              Image.asset(
+                AppAssets.CountryImage,
+                width: context.width * 0.3,
+                opacity: const AlwaysStoppedAnimation(.8),
               ),
+              48.toHorizontalSpacer(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Select your Country!", style: AppStyles.MediumLight24TextStyle),
+                  12.toVerticalSpacer(),
+                  SuperListView.separated(
+                    controller: viewModel.scrollController,
+                    itemCount: viewModel.Countries.length,
+                    itemBuilder: (context, index) {
+                      final country = viewModel.Countries[index];
+
+                      return CountryDisplayCell(
+                        country: country,
+                        isSelected: country.id! == viewModel.SelectedCountry,
+                        onTapped: () {
+                          viewModel.setSelectedCountry(country.id!);
+                          viewModel.focusNextButton();
+                        },
+                        focusNode: viewModel.countryFocusNodes[index],
+                      );
+                    },
+                    separatorBuilder: (context, index) => 12.toVerticalSpacer(),
+                    cacheExtent: 1000,
+                  ).expandWidget(),
+                  12.toVerticalSpacer(),
+                  Container(
+                    padding: 8.withAllPadding(),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color:
+                            viewModel.nextButtonFocus.hasFocus
+                                ? AppColors.LightIndicatorColor.withValues(alpha: 0.8)
+                                : Colors.transparent,
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    child: Actions(
+                      actions: <Type, Action<Intent>>{
+                        EnterButtonIntent: EnterButtonAction(
+                          () => context.push(CitySelectionPage(selectedCountry: viewModel.SelectedCountry)),
+                        ),
+                      },
+                      child: Focus(
+                        focusNode: viewModel.nextButtonFocus,
+                        onKeyEvent: (node, event) {
+                          if (event is KeyDownEvent) {
+                            if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+                              viewModel.moveFocusUp();
+                              return KeyEventResult.handled;
+                            }
+                          }
+                          return KeyEventResult.ignored;
+                        },
+                        child: PageButton(isEnabled: viewModel.SelectedCountry != "", text: "Next", onPressed: () {}),
+                      ),
+                    ),
+                  ),
+                ],
+              ).expandWidget(),
             ],
-          ).expandWidget(),
-        ],
+          ),
+        ),
       ),
     ),
   );
