22a507a2ebaabc8723010e04ec41242d837def5f
Naweed Akram <naweed@xgeno.com>
2025-05-19 06:25:51 +0100

    Commit

--- src/lib/Services/muslim_life_http_client.dart
+++ src/lib/Services/muslim_life_http_client.dart
@@ -1,15 +1,8 @@
 import 'package:http/http.dart' as http;
-import 'package:muslim_life_mosque_edition/Shared/app_constants.dart';
 
 class MuslimLifeHttpClient extends http.BaseClient {
   final http.Client _client = http.Client();
 
   @override
-  Future<http.StreamedResponse> send(http.BaseRequest request) {
-    request.headers[AppConstants.Client_ID_Key] = AppConstants.Client_ID;
-    request.headers[AppConstants.Client_Secret_Key] = AppConstants.Client_Secret;
-    request.headers[AppConstants.Client_Token_Key] = AppConstants.Client_Token;
-
-    return _client.send(request);
-  }
+  Future<http.StreamedResponse> send(http.BaseRequest request) => _client.send(request);
 }
--- src/lib/Shared/app_constants.dart
+++ src/lib/Shared/app_constants.dart
@@ -6,15 +6,6 @@ class AppConstants {
 
   static const String API_URL = "https://masjid-portal.xgeno-soft.com/v.1.0/";
 
-  static const String Client_ID_Key = "ml_client_id";
-  static const String Client_ID = "RUd/ePgfD8w+hvpBtsZm1Ygl1j7NSoOmzXmWTuFpon95j0EdhzOq1VEM7Mx5ubLu";
-
-  static const String Client_Secret_Key = "ml_client_secret";
-  static const String Client_Secret = "P8yXSJOKhDyXENbO2gR0D/NBKIiiUM7aIJ4dIIbSjpDDIFjjlzBxw77xkivPu8Yy";
-
-  static const String Client_Token_Key = "ml_client_token";
-  static const String Client_Token = "O4dN1bOTTFqS2WjBwS1f135OpuwxaL36bC060H2WcvUdw1CpTY3Uze9s5+JLhlVO";
-
   static const int VerySmallDuration = 100;
   static const int SmallDuration = 300;
   static const int MediumDuration = 600;
