6bdba5a7a19f4dfe797ceeb6aba9aaad7a1c15ab
Naweed Akram <naweed@xgeno.com>
2025-05-26 08:41:38 +0100

    Commit

--- src/lib/ViewControls/start_page/prayer_display_cell.dart
+++ src/lib/ViewControls/start_page/prayer_display_cell.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:muslim_life_mosque_edition/Shared/app_colors.dart';
 
 class PrayerDisplayCell extends StatelessWidget {
   final String prayerName;
@@ -22,8 +21,8 @@ class PrayerDisplayCell extends StatelessWidget {
       width: 130,
       decoration: BoxDecoration(
         color: isNext
-            ? const Color(0xFFFFC107).withOpacity(0.2) // background-color [cite: 20]
-            : const Color(0xFF37474F).withOpacity(0.6), // background-color [cite: 18]
+            ? const Color(0xFFFFC107).withValues(alpha: 0.2) // background-color [cite: 20]
+            : const Color(0xFF37474F).withValues(alpha: 0.6), // background-color [cite: 18]
         borderRadius: BorderRadius.circular(12.0), // border-radius [cite: 18]
         border: Border.all(
           color: isNext ? const Color(0xFFFFC107) : const Color(0xFF607D8B), // border [cite: 19, 20]
@@ -32,8 +31,8 @@ class PrayerDisplayCell extends StatelessWidget {
         boxShadow: [
           BoxShadow(
             color: isNext
-                ? const Color(0xFFFFC107).withOpacity(0.3) // box-shadow [cite: 21]
-                : Colors.black.withOpacity(0.1), // box-shadow [cite: 19]
+                ? const Color(0xFFFFC107).withValues(alpha: 0.3) // box-shadow [cite: 21]
+                : Colors.black.withValues(alpha: 0.1), // box-shadow [cite: 19]
             blurRadius: isNext ? 20.0 : 8.0,
             offset: Offset(0, isNext ? 0 : 2),
           ),
